954c94205f4a51628be78a224d7e092206618eed
Kenichiro Hiraiwa <the.world.nova@gmail.com>
2014-06-15 17:00:16 +0900

    added install fonts recipe.

---
+++ site-cookbooks/fonts/recipes/default.rb
@@ -0,0 +1,17 @@
+#
+# Cookbook Name:: fonts
+# Recipe:: default
+#
+# Copyright 2014, khiraiwa
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package 'fonts-inconsolata' do
+  action :install
+end
+
+package 'fonts-takao' do
+  action :install
+end
+
